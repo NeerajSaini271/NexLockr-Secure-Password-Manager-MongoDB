247bf14c3ca31e7eba0a55d08bacd82cb7c290cb
NeerajSaini271 <neerajkhetrisaini@gmail.com>
2025-12-08 16:33:30 +0530

    Fix port configuration and database connection

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,10 @@ const client = new MongoClient(url);
 // Database Name
 const dbName = "PassOP";
 const app = express();
-const port = 3000;
+// const port = 3000;
+
+const port = process.env.PORT || 3000;
+
 
 app.use(bodyparser.json());
 client.connect();
