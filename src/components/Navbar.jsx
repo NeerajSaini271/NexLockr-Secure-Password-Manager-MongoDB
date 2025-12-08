3aa3b1b8d345f55dbfc0ce6d2cd3a6e24c675c53
Neeraj Saini <115922287+NeerajSaini271@users.noreply.github.com>
2025-12-08 17:02:13 +0530

    Refactor Navbar to use imported GitHub icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import githubIcon from '../assets/Github.svg';
 
 const Navbar = () => {
   return (
@@ -17,7 +18,8 @@ const Navbar = () => {
           >
             <img
               className="invert cursor-pointer"
-              src="src\assets\Github.svg"
+              {/* src="src\assets\Github.svg" */}
+              src={githubIcon}
               alt="Github Logo"
             />
             <span className="font-semibold">Github</span>
